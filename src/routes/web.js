d9088553615f6be79b012403b409626f2c6dabad
assataro <45629463+assataro@users.noreply.github.com>
2022-07-30 06:50:43 -0300

    Add files via upload

--- src/routes/web.js
+++ src/routes/web.js
@@ -13,4 +13,20 @@ routes.get( "/cursos", (req, res) => {
     res.render("courses-page")
 })
 
+routes.get( "/curso/reta-final-trt-noite-ao-vivo", (req, res) => {
+    res.render("course-page")
+})
+
+routes.get( "/login", (req, res) => {
+    res.render("login")
+})
+
+routes.get( "/cadastrar", (req, res) => {
+    res.render("signup")
+})
+
+routes.get( "/checkout", (req, res) => {
+    res.render("checkout")
+})
+
 module.exports = routes
